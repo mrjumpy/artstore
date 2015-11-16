4acd55a421ef72194737a3c0ed84d7a34d9d3514
WillYou <=>
2015-11-16 12:50:18 +0000

    #4

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ActiveRecord::Base
 	belongs_to :user
 	has_many :items, class_name: "OrderItem", dependent: :destroy
     has_one  :info,  class_name: "OrderInfo", dependent: :destroy
-
+    before_create :generate_token
     accepts_nested_attributes_for :info
 
     def build_item_cache_from_cart(cart)
@@ -19,6 +19,12 @@ class Order < ActiveRecord::Base
     	self.total = cart.total_price
     	self.save
     end
+
+
+
+ 	def generate_token
+    	self.token = SecureRandom.uuid
+  	end
 end
 
 
