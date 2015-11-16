1f98988a82653a06c09e49ba3035f8c141d0eec7
WillYou <=>
2015-11-16 12:27:20 +0000

    oncourse#3

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,14 @@ Rails.application.routes.draw do
      end
    end
 
-   resources :carts
+   resources :carts do
+     collection do
+       post :checkout
+     end
+   end
+
+   resources :orders
+
 
  
 
